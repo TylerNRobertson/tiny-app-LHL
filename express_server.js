900639de6248753155ccb0d11d4ec16950830a73
Tyler Robertson <tylerr@tnrdesign.net>
2017-10-13 13:29:42 -0400

    Feature/cookies commit

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
-var express = require("express");
+const express = require("express");
 
-var app = express();
+const cookieParser = require('cookie-parser')
 
-var methodOverride = require('method-override')
+const app = express();
 
-var PORT = process.env.PORT || 8080; // default port 8080
+const methodOverride = require('method-override')
+
+const PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
@@ -12,54 +14,36 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(cookieParser());
+
+// Object Database of short and their matching long URLS
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
 // Generate random shortURL value
 function generateRandomString() {
-  var text = "";
-  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  let charset = "abcdefghijklmnopqrstuvwxyz0123456789";
   for( var i = 0; i < 5; i++ ) {
     text += charset.charAt(Math.floor(Math.random() * charset.length));
   }
   return text;
 };
 
-// Object Database of short and their matching long URLS
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-};
+// -------------------- Routing Functions -------------------------
+
+// --------------------     Get Routes    -------------------------
 
 // Sending urlDatabase data within the urls key
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+                        urls: urlDatabase,
+                        username: req.cookies["username"]
+                     };
   res.render("urls_index", templateVars);
-});
-
-// Main get request form urls_new to create/add new shortened URLs
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-// create short URL
-app.post("/urls", (req, res) => {
-  shortURL = generateRandomString();
-  longURL = req.body.longURL
-  urlDatabase[shortURL] = longURL;
-  res.redirect(longURL);
-  res.send("Ok");
-});
-
-// URL Delete
-app.post('/urls/:id/delete', function (req, res) {
-  res.send('Got a DELETE request at /user')
-  res.redirect("../views/urls_index")
-})
-
-// New URL update
-app.post("/urls/:id", (req, res) => {
-    urlDatabase[req.params.id] = {
-      longURL: req.body.newURL }
-    res.send('Got a url change request <html><a href="/urls">Go back to URLS</a></html>')
-    res.redirect('/urls')
 });
 
 // Get the Long URL
@@ -68,12 +52,62 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+// Main get request form urls_new to create/add new shortened URLs
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+                      username: req.cookies["username"]
+                     };
+  res.render("urls_new", templateVars);
+});
+
 // Assign an object key to both the short and long URL to be accessed by urls_show
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
-                       longURL: urlDatabase[req.params.id]
+                       longURL: urlDatabase[req.params.id],
+                       username: req.cookies["username"]
                      };
   res.render("urls_show", templateVars);
+});
+
+// --------------------    Post Routes    -------------------------
+
+// create short URL
+app.post("/urls", (req, res) => {
+  shortURL = generateRandomString();
+  longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
+  res.send("Ok");
+});
+
+// URL Delete
+app.post('/urls/:id/delete', function (req, res) {
+  delete urlDatabase[req.params.id];
+  res.send('Deleted! <html><a href="/urls">Go back to URLS</a></html>')
+  res.redirect("/urls")
+})
+
+// Change Long URL
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL
+    res.redirect('/urls')
+});
+
+// Login cookie
+app.post('/login', function (req, res) {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
+})
+// Remove login cookie
+app.post('/logout', function (req, res) {
+  res.clearCookie('username');
+  res.redirect('/urls')
+})
+
+// Logout
+app.post('/logout', (req, res) => {
+  req.session.user_id = null;
+  res.redirect('/');
 });
 
 // Telling the app to listen to the defined port and only that port
